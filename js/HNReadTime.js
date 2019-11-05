1cacc53867a7a39e5e517bdcf74c733c6b0c5ef6
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-11-05 22:29:06 +0100

    Add basic crawler

--- js/HNReadTime.js
+++ js/HNReadTime.js
@@ -2,26 +2,34 @@ $(document).ready(function(){
     let eng = HNReadTime();
 
     let port = chrome.runtime.connect({name: "crawler"});
-    port.postMessage({url: "https://www.theguardian.com/world/2019/nov/01/undercover-reporter-reveals-life-in-a-polish-troll-farm"})
+    port.postMessage({url: "https://medium.com/@marksaroufim/how-to-turn-physics-into-an-optimization-problem-11b3fbf83062"})
     port.onMessage.addListener( msg => {
-        console.log(body);
-        let innerText = body.text();
-        let wordscount = innerText.match(/\S+/g).length
-        console.log(wordscount);
+        console.log(MetricsTask(msg.resp, 265));
     } );
 })
 
 const HNReadTime = (options) => {
-    let opts = {
-        wpm: 265
-    };
-    Object.assign(opts, options);
+    let _opts = {wpm: 265};
+    Object.assign(_opts, options);
+};
 
-    let articles = $('.title');
+const MetricsTask = (htmlString, wpm) => {
+    let _parser = new DOMParser();
+    let _html = _parser.parseFromString(htmlString, 'text/html');
+    let _body = $(_html.getElementsByTagName('body')[0]);
+    _body.find('script, style, link, img').remove();
 
-    function computeReadTime(content){
+    let _wordsCount = (body) => body.text().match(/\S+/g).length
+    let _wordsReadTime = (wordsCount) => Math.round(wordsCount/wpm);
+    let _crawledReadTime = (body) => {
+        let found = body.text().match(/\d+\smin.?read|read\stime\s\d+/i);
+        if (found)
+            found = found[0].match(/\d+/)[0];
+        return found;
     }
 
     return {
+        wordsReadTime: _wordsReadTime(_wordsCount(_body)),
+        crawledReadTime: _crawledReadTime(_body)
     }
-};
+}
