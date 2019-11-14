5483d8538e9a44187a58a1d07a7021c6b66ba38b
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-11-14 21:25:27 +0100

    Add basic options rendering

--- js/options.js
+++ js/options.js
@@ -1,18 +1,28 @@
 $(document).ready(() => chrome.storage.sync.get(null, opts =>{
     
-    let settingsForm = `
-        <form>
-            <
-        </form>
-    `;
 
+    let makeInput = (id, value='', classes=[], type='text') => 
+        $('<input>')
+        .attr('id' , id)
+        .attr('type', type)
+        .attr('value', value)
+        .addClass(classes.concat('form-control').join(' '))
 
-    for (let k in opts){
-        if (opts[k].is_advanced)
-            continue;
+    let makeLabel = (forwho, content, classes=[]) =>
+        $('<label></label>')
+        .attr('for', forwho)
+        .addClass(classes.concat('col-form-label'))
+        .text(content)
 
-        let opt = opts[k]
+    let makeFormGroup = (label, input) =>
+        $("<div class='form-group row'></div>")
+        .append(label)
+        .append(input)
 
-        $("#options").append(setting);
-    }
+    let makeSettings = opts => Object.keys(opts).map(k => makeFormGroup(
+        makeLabel(k, opts[k].title, ['col-8']),
+        makeInput(k, opts[k].value, ['col-4'])
+    ));
+
+    $('#options').append(makeSettings(opts));
 }));
