7d49a56ac7ff0589b6f61fab1bf8f381be1de10f
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2019-11-15 19:13:03 +0100

    Refactor options rendering

--- js/options.js
+++ js/options.js
@@ -1,28 +1,43 @@
-$(document).ready(() => chrome.storage.sync.get(null, opts =>{
-    
+$(document).ready(() => {
+    let render = OptionsRender();
+    chrome.storage.sync.get(null, opts =>{
+        render.render(opts, 'container');
+    });
+});
 
-    let makeInput = (id, value='', classes=[], type='text') => 
+const OptionsRender = () => {
+    let factory = FormFactory();
+
+    return {
+        render: (opts, idTarget) => $('#'+idTarget).append(Object.keys(opts).map(k => factory.makeFormGroup(
+            factory.makeLabel(k, opts[k].title, ['col-8']),
+            factory.makeInput(k, opts[k].value, ['col-4'])
+        )))
+    }
+}
+
+const FormFactory = () => ({
+    makeInput: (id, value='', classes=[], type='text') => 
         $('<input>')
         .attr('id' , id)
         .attr('type', type)
         .attr('value', value)
-        .addClass(classes.concat('form-control').join(' '))
+        .addClass(classes.concat('form-control').join(' ')),
 
-    let makeLabel = (forwho, content, classes=[]) =>
+    makeLabel: (forwho, content, classes=[]) =>
         $('<label></label>')
         .attr('for', forwho)
         .addClass(classes.concat('col-form-label'))
-        .text(content)
+        .text(content),
 
-    let makeFormGroup = (label, input) =>
+    makeFormGroup: (label, input) =>
         $("<div class='form-group row'></div>")
         .append(label)
-        .append(input)
+        .append(input),
 
-    let makeSettings = opts => Object.keys(opts).map(k => makeFormGroup(
-        makeLabel(k, opts[k].title, ['col-8']),
-        makeInput(k, opts[k].value, ['col-4'])
-    ));
-
-    $('#options').append(makeSettings(opts));
-}));
+    makeForm: (...formGroups) => {
+        let form = $("<form></form>");
+        formGroups.forEach(x => $(form).append(x));
+        return $(form);
+    }
+})
